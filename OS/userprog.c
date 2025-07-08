de7af11141cd7d7885c7edea96b5a3c5e6f3c0f6
aamos-k <aamoskaukua8@gmail.com>
2025-07-08 12:47:40 +0300

    Delete OS directory

--- OS/userprog.c
+++
@@ -1,9 +0,0 @@
-#include "syscalls.h"
-
-void load_user_program() {
-    const char* msg  = "Hello, kernel!\n";
-    const char* name = "hello";
-    syscall(2, (int)name, (int)msg, sizeof("Hello, kernel!\n") - 1);
-    syscall(1,0,0,0); // sys_exit
-}
-
