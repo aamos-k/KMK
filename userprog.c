8ff3a59b6a421747ef9040fb5e3c6ea68f83ef4f
Aamos <aamoskaukua8@gmail.com>
2025-07-08 20:37:07 +0300

    Update userprog.c

--- userprog.c
+++ userprog.c
@@ -1,9 +1,12 @@
 #include "syscalls.h"
 
+char buffer[64];
+
 void load_user_program() {
     const char* msg  = "Hello, kernel!\n";
     const char* name = "hello";
     syscall(2, (int)name, (int)msg, sizeof("Hello, kernel!\n") - 1);
+    syscall(3, (int)name, (int)buffer, sizeof(buffer) - 1);
     syscall(1,0,0,0); // sys_exit
 }
 
