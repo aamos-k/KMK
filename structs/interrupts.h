c99fdfec213f2f35ba0dddbcecaa80e64fea62e4
aamos-k <aamoskaukua8@gmail.com>
2025-07-08 12:53:31 +0300

    Update interrupts.h

--- structs/interrupts.h
+++ structs/interrupts.h
@@ -7,14 +7,5 @@
 
 void register_interrupt_handler(int n, void (*handler)(struct registers*));
 
-typedef struct registers {
-    uint32_t ds;
-    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-    uint32_t int_no, err_code;
-    uint32_t eip, cs, eflags, useresp, ss;
-} registers_t;
-
-
-
 #endif
 
